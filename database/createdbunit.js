09c649e83ce387f529d8dc2070782f0253bb10e5
Dan Kahle <dan.kahle@gmail.com>
2018-01-28 06:03:07 -0800

    working on unit data

--- database/createdbunit.js
+++ database/createdbunit.js
@@ -12,16 +12,166 @@ db.users.createIndex({id: 1}, {unique: true});
 db.users.createIndex({name: 1});
 
 var labelOne = {id: 'c62dac5b-97d8-53a5-9989-cb2f779bc5e1', name: 'label one'};
+var labelTwo = {id: 'c62dac5b-97d8-53a5-9989-cb2f779bc5e2', name: 'label two'};
 var labelOneCarl = {id: 'c62dac5b-97d8-53a5-9989-cb2f779bc4e1', name: 'label one carl'};
-var janeId = 'c62dac5b-97d8-53a5-9989-cb2f779bc6e1';
 var dankUserId = 'c62dac5b-97d8-53a5-9989-cb2f779bc7e1';
 var dankCompany = 'dank co';
 var carlUserId = 'c62dac5b-97d8-53a5-9989-cb2f779bc7e2';
 
 var contacts = [
-  {userId: dankUserId, id: janeId, name: 'jane', labels:[labelOne], emails: [], phones: [], addresses: [], websites: []},
-  {userId: dankUserId, id: 'c62dac5b-97d8-53a5-9989-cb2f779bc6e2', name: 'brenda', labels:[], emails: [], phones: [], addresses: [], websites: []},
-  {userId: dankUserId, id: 'c62dac5b-97d8-53a5-9989-cb2f779bc6e3', name: 'martha', labels:[labelOne], emails: [], phones: [], addresses: [], websites: []},
+  {
+    userId: dankUserId,
+    id: 'c62dac5b-97d8-53a5-9989-cb2f779bc6e1',
+    name: 'jane',
+    company: 'Jane Co',
+    jobTitle: 'Manager',
+    labels:[labelOne, labelTwo],
+    "emails" : [
+      {
+        "email" : "jane1@gmail.com",
+        "label" : "Work"
+      },
+      {
+        "email" : "jane2@gmail.com",
+        "label" : "Home"
+      }
+    ],
+    "phones" : [
+      {
+        "prefix" : "1",
+        "phone" : "111-222-3333",
+        "label" : "Work"
+      },
+      {
+        "prefix" : "55",
+        "phone" : "112-222-3333",
+        "label" : "Mobile"
+      }
+    ],
+    "addresses" : [
+      {
+        "address" : "952 NE Lovell St. Hillsboro, OR 97124",
+        "label" : "Home"
+      },
+      {
+        "address" : "10260 SW Greenburg Rd #600, Tigard, OR 97223",
+        "label" : "Work"
+      }
+    ],
+    "websites" : [
+      {
+        "website" : "https://www.google.com",
+        "label" : "google"
+      },
+      {
+        "website" : "www.weather.com",
+        "label" : "weather"
+      }
+    ],
+    "notes" : "notes first line\nnotes second line\nnotes third line"
+  },
+  {
+    userId: dankUserId,
+    id: 'c62dac5b-97d8-53a5-9989-cb2f779bc6e2',
+    name: 'brenda',
+    company: 'Brenda Co',
+    jobTitle: 'QA',
+    labels:[labelTwo],
+    "emails" : [
+      {
+        "email" : "brenda1@gmail.com",
+        "label" : "Work"
+      },
+      {
+        "email" : "brenda2@gmail.com",
+        "label" : "Home"
+      }
+    ],
+    "phones" : [
+      {
+        "prefix" : "1",
+        "phone" : "111-222-3333",
+        "label" : "Work"
+      },
+      {
+        "prefix" : "55",
+        "phone" : "112-222-3333",
+        "label" : "Mobile"
+      }
+    ],
+    "addresses" : [
+      {
+        "address" : "952 NE Lovell St. Hillsboro, OR 97124",
+        "label" : "Home"
+      },
+      {
+        "address" : "10260 SW Greenburg Rd #600, Tigard, OR 97223",
+        "label" : "Work"
+      }
+    ],
+    "websites" : [
+      {
+        "website" : "https://www.google.com",
+        "label" : "google"
+      },
+      {
+        "website" : "www.weather.com",
+        "label" : "weather"
+      }
+    ],
+    "notes" : "notes first line\nnotes second line\nnotes third line"
+  },
+  {
+    userId: dankUserId,
+    id: 'c62dac5b-97d8-53a5-9989-cb2f779bc6e3',
+    name: 'martha',
+    company: 'Martha Co',
+    jobTitle: 'Engineer',
+    labels:[labelOne],
+    "emails" : [
+      {
+        "email" : "martha1@gmail.com",
+        "label" : "Work"
+      },
+      {
+        "email" : "martha2@gmail.com",
+        "label" : "Home"
+      }
+    ],
+    "phones" : [
+      {
+        "prefix" : "1",
+        "phone" : "111-222-3333",
+        "label" : "Work"
+      },
+      {
+        "prefix" : "55",
+        "phone" : "112-222-3333",
+        "label" : "Mobile"
+      }
+    ],
+    "addresses" : [
+      {
+        "address" : "952 NE Lovell St. Hillsboro, OR 97124",
+        "label" : "Home"
+      },
+      {
+        "address" : "10260 SW Greenburg Rd #600, Tigard, OR 97223",
+        "label" : "Work"
+      }
+    ],
+    "websites" : [
+      {
+        "website" : "https://www.google.com",
+        "label" : "google"
+      },
+      {
+        "website" : "www.weather.com",
+        "label" : "weather"
+      }
+    ],
+    "notes" : "notes first line\nnotes second line\nnotes third line"
+  },
 
   {userId: carlUserId, id: 'c62dac5b-97d8-53a5-9989-cb2f779bc3e1', name: 'carl cont1', labels:[labelOneCarl], emails: [], phones: [], addresses: [], websites: []},
   {userId: carlUserId, id: 'c62dac5b-97d8-53a5-9989-cb2f779bc3e2', name: 'carl cont2', labels:[], emails: [], phones: [], addresses: [], websites: []},
