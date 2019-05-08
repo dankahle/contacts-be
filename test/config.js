03622faaefafefe2626fb1ff87081cec3ad4680f
Dan Kahle <dan.kahle@gmail.com>
2019-05-08 14:47:16 -0700

    change dev database back to contacts from unit

--- test/config.js
+++ test/config.js
@@ -1,7 +1,7 @@
 const expect = require('chai').expect;
 const dev = require('../config/development');
 
-describe.only('dev config tests', () => {
+describe('dev config tests', () => {
 
   it('should have database set to contacts', () => {
     expect(dev.database).to.equal('mongodb://localhost/contacts');
